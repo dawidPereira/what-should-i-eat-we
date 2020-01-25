022cbf7d8e714a84cccbcb93f49a2eef290f5046
Dawid Pereira <pereiradawid@outlook.com>
2020-01-25 10:11:05 +0100

    Added text editor

--- src/components/back-office/add-recipe/add-recipe-card.vue
+++ src/components/back-office/add-recipe/add-recipe-card.vue
@@ -15,8 +15,13 @@
                     </v-row>
                 </v-col>
                 <v-subheader class="font-weight-bold header ml-n4">Opis:</v-subheader>
-                <v-col>
-                   
+                <v-col class="ml-n4">
+                    <div>
+                    <tiptap-vuetify
+                            v-model="content"
+                            :extensions="extensions"
+                    />
+                    </div>
                 </v-col>
             </v-form>
         </v-col>
@@ -25,18 +30,47 @@
 
 <script>
     import TealTextField from "@/components/search/teal-text-field";
+    import { TiptapVuetify, Heading, Bold, Italic, Strike, Underline, Code, Paragraph, BulletList, OrderedList, ListItem, Link, Blockquote, HardBreak, HorizontalRule, History } from 'tiptap-vuetify'
+    import 'tiptap-vuetify/dist/main.css'
     export default {
         name: "add-recipe-card",
-        components: {TealTextField},
+        components: {TealTextField, TiptapVuetify},
         data: () => ({
             name: "Genialny przepis",
-            items: [1,2,3,4,5]
+            items: [1,2,3,4,5],
+            editor: null,
+            extensions: [
+                History,
+                Blockquote,
+                Link,
+                Underline,
+                Strike,
+                Italic,
+                ListItem,
+                BulletList,
+                OrderedList,
+                [Heading, {
+                    options: {
+                        levels: [1, 2, 3]
+                    }
+                }],
+                Bold,
+                Code,
+                HorizontalRule,
+                Paragraph,
+                HardBreak
+            ],
+            // starting editor's content
+            content: `
+      <h1>Yay Headlines!</h1>
+      <p>All these <strong>cool tags</strong> are working now.</p>
+    `
         }),
         methods:{
             nameChanged(value) {
                 this.name = value;
             }
-        }
+        },
     }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+import App from "@/App.vue";
+import router from "@/router";
+import Vuetify from 'vuetify'
+// import plugin
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+// don't forget to import CSS styles
+import 'tiptap-vuetify/dist/main.css'
+// Vuetify's CSS styles 
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.config.productionTip = false;
+
+// Vuetify Object (as described in the Vuetify 2 documentation)
+const vuetify = new Vuetify();
+
+// use Vuetify's plugin
+Vue.use(Vuetify);
+// use this package's plugin
+Vue.use(TiptapVuetifyPlugin, {
+  // the next line is important! You need to provide the Vuetify Object to this place.
+  vuetify, // same as "vuetify: vuetify"
+  // optional, default to 'md' (default vuetify icons before v2.0.0)
+  iconsGroup: 'mdi'
+});
 
 new Vue({
   router,
